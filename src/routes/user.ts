aaf6e5b99df1b9c25e2fd3bc7b032e8f409f454d
Vikram <vikram@shortcastle.com>
2022-10-22 03:22:33 +0530

    Login endpoint

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -7,50 +7,52 @@ const prisma = new PrismaClient();
 export const UserRouter = express.Router();
 
 function generateAccessToken(user: any) {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  return jwt.sign(user, process.env.JWT_SECRET);
 }
 
 UserRouter.post("/login", async (req, res, next) => {
   try {
     const { username, password } = req.body;
+
     const user = await prisma.users.findFirst({ where: { username } });
+
     if (!user) {
-      return res.sendStatus(401);
+      return res.send("401");
     }
     const isAuthorized = await bcrypt.compare(password, user.password);
     if (!isAuthorized) {
-      return res.sendStatus(401);
+      return res.send("401");
     }
-    return res.sendStatus(200);
 
-    // const accessToken = generateAccessToken(user);
-    // res.json({ accessToken: accessToken });
+    const accessToken = generateAccessToken(user);
+    return res.json({ accessToken: accessToken });
   } catch (e) {
-    return next();
+    console.error(e);
+    return next(e);
   }
 });
 
-UserRouter.post("/", async (req, res, next) => {
+UserRouter.post("/signup", async (req, res, next) => {
   try {
     const { username, firstName, lastName, password, email } = req.body;
 
     if (!username) {
-      return res.sendStatus(400).send("username required");
+      return res.status(400).send("username required");
     }
 
     if (firstName) {
-      return res.sendStatus(400).send("firstName required");
+      return res.status(400).send("firstName required");
     }
 
     if (!lastName) {
-      return res.sendStatus(400).send("lastName required");
+      return res.status(400).send("lastName required");
     }
 
     if (!password) {
-      return res.sendStatus(400).send("password required");
+      return res.status(400).send("password required");
     }
     if (!email) {
-      return res.sendStatus(400).send("email required");
+      return res.status(400).send("email required");
     }
 
     const salt = await bcrypt.genSalt(10);
