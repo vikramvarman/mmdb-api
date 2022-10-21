12f4eedec797f26cad057c2224b3a5c4d2e4477a
Vikram <vikram@shortcastle.com>
2022-10-22 03:02:57 +0530

    signup endpoint

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,19 +1,74 @@
 import express from "express";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
 
-export const UserRouter = express.Router()
+const prisma = new PrismaClient();
+export const UserRouter = express.Router();
 
-function generateAccessToken(user:any) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+function generateAccessToken(user: any) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+}
+
+UserRouter.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await prisma.users.findFirst({ where: { username } });
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    const isAuthorized = await bcrypt.compare(password, user.password);
+    if (!isAuthorized) {
+      return res.sendStatus(401);
+    }
+    return res.sendStatus(200);
+
+    // const accessToken = generateAccessToken(user);
+    // res.json({ accessToken: accessToken });
+  } catch (e) {
+    return next();
   }
+});
 
-UserRouter.post('/login', (req, res) => {
-    // Authenticate User
-  
-    const username = req.body.username
-    const user = { name: username }
-  
-    const accessToken = generateAccessToken(user)
-    
-    res.json({ accessToken: accessToken })
-  })
+UserRouter.post("/", async (req, res, next) => {
+  try {
+    const { username, firstName, lastName, password, email } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400).send("username required");
+    }
+
+    if (firstName) {
+      return res.sendStatus(400).send("firstName required");
+    }
+
+    if (!lastName) {
+      return res.sendStatus(400).send("lastName required");
+    }
+
+    if (!password) {
+      return res.sendStatus(400).send("password required");
+    }
+    if (!email) {
+      return res.sendStatus(400).send("email required");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.users.create({
+      data: {
+        firstName,
+        username,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    delete user.password;
+    return res.json({ record: user });
+  } catch (e) {
+    return next(e);
+  }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 // this will load app which contains our main structure and logic
-import dotenv from 'dotenv';
-import app from './app'
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
+
+dotenvExpand.expand(dotenv.config());
+
+console.log("app initiated");
+
+import app from "./app";
 
 // use this line to get port from environment variable
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
-  console.log('listening on port ' + PORT)
-})
-
+  console.log("listening on port " + PORT);
+});
